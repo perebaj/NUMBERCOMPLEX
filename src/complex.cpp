92e9cffde23e417178f82227473e2978a42f9712
Jonathan <perebaj@gmail.com>
2018-10-24 08:17:49 -0200

    ++POS + Soma com um inteiro a esquerda do operador

--- src/complex.cpp
+++ src/complex.cpp
@@ -8,67 +8,90 @@ class Complex
     int real, imag;
 
   public:
-
+    //Constrututor
     Complex(int real = 0, int imag = 0)
     {
         this->real = real;
         this->imag = imag;
     }
-    /*
-    Tipo de retorno tem que ser igual ao q eu estou sobrecarregando, no caso, um objeto complexo
-    */
 
-    Complex operator+(const Complex &c)
-    {
-        /*
-        Imprimindo real e imag dentro da função, para mostrar que o objeto que esta a direita do operador sobrecaregado 
-        é o objeto que esta sendo "Acessado", o da esquerda no caso, esta sendo passado por referência.
-        acessado.
-        EX:        
-        c3 = c1 + c2 < C2 ESTA SENDO PASSADO POR REFERENCIA NESSA SINTAXE.
+    const Complex operator+(const Complex &c) const;
+    const Complex operator+(int a) const;
+    const Complex operator*(const Complex &c) const;
+    const Complex operator++(int) {
+        Complex c1(*this);
+        real++; 
+        return c1;
+    }
+    //Funçoes Friends :
+
+    friend std::ostream &operator<<(std::ostream &, Complex &);
+    friend const Complex operator+(int a , const Complex &c );
+};
+const Complex operator+(int a , const Complex &c){
+    return Complex(a + c.real, c.imag);
+}
+/*
+    Tipo de retorno tem que ser igual ao q eu estou sobrecarregando, no caso, um objeto complexo
+   
+    Imprimindo real e imag dentro da função, para mostrar que o objeto que esta a direita do operador sobrecaregado 
+    é o objeto que esta sendo "Acessado", o da esquerda no caso, esta sendo passado por referência.
+    acessado.
+    EX:        
+    c3 = c1 + c2 < C2 ESTA SENDO PASSADO POR REFERENCIA NESSA SINTAXE
               ^AQUI C1 ESTA A DIREITA DO OPERADOR SOBRECARREGADO (+).
         */
-        return Complex(this->real + c.real, this->imag + c.imag);
-    }
-
-    Complex operator+(int a);
-    Complex operator*(const Complex &c)
-    {
-        return Complex(this->real * c.real, this->imag * c.imag);
-    }
-    friend std::ostream &operator<<(std::ostream &, Complex &);
-};
-/*
-Sobrecarga do operador <<
-*/
-std::ostream &operator<<(std::ostream &output, Complex &c){
-    output << c.real << " + " << c.imag << "i" << std::endl;
-    return output;
+const Complex Complex::operator+(const Complex &c) const 
+{
+    return Complex(this->real + c.real, this->imag + c.imag);
 }
 
 /*
     Sobrecarga do operador +, para somar o objeto com um numero inteiro
     */
-Complex Complex::operator+(int a)
+const Complex Complex::operator+(int a) const 
 { //{tipo de retorno} {classe referida} :: NOME DA FUNÇÃO
     return Complex(real + a, imag);
 }
+/*
+    Sobrecarga do operador (*) para multiplicar dois objetos
+*/
+const Complex Complex::operator*(const Complex &c) const
+{
+    return Complex(this->real * c.real, this->imag * c.imag);
+}
+
+/*
+Sobrecarga do operador <<
+*/
+std::ostream &operator<<(std::ostream &output, Complex &c)
+{
+    output << c.real << " + " << c.imag << "i" << std::endl;
+    return output;
+}
+
 
 int main()
 {
 
     Complex c1(2, 3), c2(3, 7), c4(4, 6), c3;
 
-
     std::cout << "RESULTADO DA SOMA: " << std::endl;
     c3 = c1 + c4; // O QUE ESTA ACONTECENDO AQUI É EQUIVALENTE À c3 = c1.operator+(c2);
     std::cout << "c3 = " << c3;
     std::cout << "<==================================>" << std::endl;
     std::cout << "RESULTADO DA SOMA COM UM INTEIRO: " << std::endl;
-    c1 = c1 + 4;  //c1 = c1.operator+(4);
+    c1 = c1 + 4; //c1 = c1.operator+(4);
     std::cout << "c1 = " << c1;
     std::cout << "<==================================>" << std::endl;
     std::cout << "RESULTADO DA MULTIPLICAÇAO: " << std::endl;
     c1 = c1 * c2; //c1 = c1.operator*(c2);
     std::cout << "c1 = " << c1;
+
+    c1 = 4 + c1;
+
+    std::cout << c1;
+
+    c3++;
+    std::cout<<c3<<std::endl;
 }
